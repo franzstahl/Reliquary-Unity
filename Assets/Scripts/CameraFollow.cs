848c60b582183091ef6b3cd6761342a3b12b4187
Francisco <francisco.garzon0718@gmail.com>
2026-07-10 19:26:01 -0300

    Added Camera Follow

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,13 @@
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    [SerializeField] private Transform playerTransform;
+   
+    
+    private void LateUpdate()
+    {
+        transform.position = new Vector3(playerTransform.position.x, playerTransform.position.y, transform.position.z);
+    }
+}
