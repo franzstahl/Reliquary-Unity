014963d0e93c3529bfe3b8ecf497643148482f4f
Francisco <francisco.garzon0718@gmail.com>
2026-07-09 23:50:01 -0300

    Improving movement

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -3,9 +3,13 @@ using UnityEngine;
 public class Movement : MonoBehaviour
 {
    [SerializeField] private float speed;
-    private Rigidbody2D rb;
-    private Animator anim;
-    private SpriteRenderer sr;
+   [SerializeField] private Transform groundCheck; 
+   [SerializeField] private float groundCheckRadius;
+   [SerializeField] private LayerMask groundLayer; // LayerMask to specify which layers are considered ground
+   [SerializeField] private float jumpForce; // Force applied to the player when jumping
+   private Rigidbody2D rb;
+   private Animator anim;
+   private SpriteRenderer sr;
 
 
     
@@ -19,7 +23,9 @@ public class Movement : MonoBehaviour
     
     void Update()
     {
-        anim.SetBool("isGrounded", true);
+        bool isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer); // Check if the player is grounded using OverlapCircle
+        anim.SetBool("isGrounded", isGrounded); // Set the "isGrounded" parameter in the Animator based on whether the player is grounded
+
         float horizontalInput = Input.GetAxisRaw("Horizontal"); // Get horizontal input from player
         anim.SetFloat("Speed", Mathf.Abs(horizontalInput)); // Set the "Speed" parameter in the Animator based on the absolute value of horizontal input
         rb.linearVelocity = new Vector2(horizontalInput * speed, rb.linearVelocity.y);  // Set the linear velocity of the Rigidbody2D based on horizontal input and speed
@@ -31,6 +37,12 @@ public class Movement : MonoBehaviour
         else if (horizontalInput > 0)
         {
             sr.flipX = false; // Reset the sprite flip when moving right
+        }
+
+        if (isGrounded && Input.GetKeyDown(KeyCode.W)) // Check if the player is grounded and the jump key (W) is pressed
+        {
+            rb.linearVelocity = new Vector2(horizontalInput * speed, jumpForce);
+            anim.SetBool("isGrounded", false);
         }
     }
 }
