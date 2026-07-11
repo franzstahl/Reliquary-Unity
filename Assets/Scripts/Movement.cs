c1d720310d840387172942d41b0633bd354de84c
Francisco <francisco.garzon0718@gmail.com>
2026-07-10 23:39:40 -0300

    Fixed camera

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -44,5 +44,7 @@ public class Movement : MonoBehaviour
             rb.linearVelocity = new Vector2(horizontalInput * speed, jumpForce);
             anim.SetBool("isGrounded", false);
         }
+
+        Debug.Log("isGrounded: " + isGrounded + " | Speed: " + Mathf.Abs(horizontalInput));
     }
 }
