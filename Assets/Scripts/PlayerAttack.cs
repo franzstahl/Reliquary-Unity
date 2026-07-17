e7ee7e33bcf9a941033b586b8d9c18ee42527bed
Francisco <francisco.garzon0718@gmail.com>
2026-07-16 22:39:28 -0300

    Added jump sound

--- Assets/Scripts/PlayerAttack.cs
+++ Assets/Scripts/PlayerAttack.cs
@@ -1,16 +1,27 @@
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class PlayerAttack : MonoBehaviour
 {
-    
+    [SerializeField] private float attackCooldown;
+    [SerializeField] private float lastAttackTime;
+    [SerializeField] private AudioClip attackSound;
+
+    private AudioSource audioSource;
+
     private void Start()
     {
-        
+        audioSource = GetComponent<AudioSource>();
     }
 
    
    private void Update()
     {
-        
+        if (lastAttackTime + attackCooldown < Time.time && Input.GetMouseButtonDown(0))
+        {
+            lastAttackTime = Time.time;
+
+
+        }
     }
 }
