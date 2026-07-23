397af2ad2b3e6ef03ca6e636821d8f8587d0d533
Francisco <francisco.garzon0718@gmail.com>
2026-07-23 17:24:07 -0300

    Progressing tilemap

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -9,14 +9,14 @@ public class CameraFollow : MonoBehaviour
     [SerializeField] private float maxX; // Maximum X position for the camera
     [SerializeField] private float minX; // Minimum X position for the camera
 
-    private Camera cam;
+    private Camera cam; 
 
     private void Awake()
     {
         cam = GetComponent<Camera>();
     }
 
-    private void LateUpdate()
+    private void LateUpdate() 
     {
         if (playerTransform == null) return; // If playerTransform is not assigned, exit the method to avoid errors
 
@@ -27,6 +27,6 @@ public class CameraFollow : MonoBehaviour
 
         float clampedX = Mathf.Clamp(playerTransform.position.x, minXAllowed, maxXAllowed); // Clamp the camera's X position to stay within the defined min and max boundaries
 
-        transform.position = new Vector3(clampedX, transform.position.y , transform.position.z); // Update the camera's position to follow player with a vertical offset
+        transform.position = new Vector3(clampedX, transform.position.y , transform.position.z); // Update the camera's position to follow player while keeping Y and Z positions unchanged
     }
 }
